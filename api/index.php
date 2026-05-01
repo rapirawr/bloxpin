529c870be52bc3e9fc3f165e23881e68d4d3e7f1
rapirawr <animekayangg313@gmail.com>
2026-05-01 11:26:56 +0700

    add error reporting

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
 
-// Forward Vercel requests to the public/index.php
-require __DIR__ . '/../public/index.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Fatal Error Caught:</h1>";
+    echo "<pre>" . $e->getMessage() . "</pre>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
