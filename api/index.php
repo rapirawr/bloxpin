f1271046287b949ea81f730a922aaf923e69ce39
rapirawr <animekayangg313@gmail.com>
2026-05-01 11:32:54 +0700

    fix vercel tmp folder creation

--- api/index.php
+++ api/index.php
@@ -4,6 +4,20 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Ensure Laravel can write to /tmp
+$compiledViews = '/tmp/storage/framework/views';
+if (!is_dir($compiledViews)) {
+    mkdir($compiledViews, 0777, true);
+}
+
+// Override Laravel Cache paths to /tmp dynamically
+$_ENV['VIEW_COMPILED_PATH'] = $compiledViews;
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE']   = '/tmp/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE']   = '/tmp/bootstrap/cache/routes.php';
+$_ENV['APP_EVENTS_CACHE']   = '/tmp/bootstrap/cache/events.php';
+
 try {
     require __DIR__ . '/../public/index.php';
 } catch (\Throwable $e) {
