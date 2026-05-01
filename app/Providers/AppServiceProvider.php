5d4749d668294e59db48e19c2509193f9b0fb51c
rapirawr <animekayangg313@gmail.com>
2026-05-01 13:34:05 +0700

    revert system to stable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local' || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') || isset($_ENV['VERCEL'])) {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
