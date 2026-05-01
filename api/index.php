2d63e5a72230f1a65c3adfc886ea7cdb4a7e4aca
rapirawr <animekayangg313@gmail.com>
2026-05-01 11:34:35 +0700

    remove bad cache overrides

--- api/index.php
+++ api/index.php
@@ -12,11 +12,6 @@ if (!is_dir($compiledViews)) {
 
 // Override Laravel Cache paths to /tmp dynamically
 $_ENV['VIEW_COMPILED_PATH'] = $compiledViews;
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
-$_ENV['APP_CONFIG_CACHE']   = '/tmp/bootstrap/cache/config.php';
-$_ENV['APP_ROUTES_CACHE']   = '/tmp/bootstrap/cache/routes.php';
-$_ENV['APP_EVENTS_CACHE']   = '/tmp/bootstrap/cache/events.php';
 
 try {
     require __DIR__ . '/../public/index.php';
